16cbc0c206700bbdfbae5b4de85de8562ed480e4
oyvindis <oyvindis>
2015-11-10 20:28:39 +0100

    update

--- dist/server.js
+++ dist/server.js
@@ -1,34 +1,10 @@
 var express = require('express');
-var path = require('path');
-var httpProxy = require('http-proxy');
-
-var proxy = httpProxy.createProxyServer();
 var app = express();
-
 var isProduction = process.env.NODE_ENV === 'production';
 var port = isProduction ? process.env.PORT : 3000;
-var publicPath = path.resolve(__dirname, 'public');
 
-app.use(express.static(publicPath));
-
-if (!isProduction) {
-    var bundle = require('./server/bundle.js');
-    bundle();
-    app.all('/build/*', function (req, res) {
-        proxy.web(req, res, {
-            target: 'http://localhost:8080'
-        });
-    });
-}
-
-app.get('*.png', function (req, res) {
-    res.sendFile(publicPath + req.path);
-});
-
-proxy.on('error', function (e) {
-    console.log('Could not connect to proxy, please try again...');
-});
+app.use(express.static(__dirname));
 
 app.listen(port, function () {
-    console.log('Server running on port ' + port);
+    console.log('Fremoverlent demo lytter på port ' + port);
 });
